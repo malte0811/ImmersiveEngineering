cb1df98288d8db5a7f931cc47a10ab735cdc6d4a
malte0811 <malte0811@web.de>
2019-03-24 14:08:34 +0100

    Updated the debug coremod

--- src/main/java/blusunrize/immersiveengineering/common/asm/IEClassTransformer.java
+++ src/main/java/blusunrize/immersiveengineering/common/asm/IEClassTransformer.java
@@ -15,7 +15,7 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
-import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.function.Consumer;
 
@@ -29,11 +29,43 @@ public class IEClassTransformer implements IClassTransformer
 	static
 	{
 		Consumer<MethodNode> addThreadingCall = (m)->{
-			InsnList insert = new InsnList();
-			insert.add(new LdcInsnNode("Server thread"));
-			insert.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "blusunrize/immersiveengineering/common/asm/ThreadLogger", "checkForThread",
-					"(Ljava/lang/String;)V", false));
-			m.instructions.insertBefore(m.instructions.get(0), insert);
+			{
+				InsnList insert = new InsnList();
+				insert.add(new LdcInsnNode("Server thread"));
+				insert.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "blusunrize/immersiveengineering/common/asm/ThreadLogger", "checkForThread",
+						"(Ljava/lang/String;)V", false));
+				m.instructions.insertBefore(m.instructions.get(0), insert);
+			}
+			ListIterator<AbstractInsnNode> it = m.instructions.iterator();
+			AbstractInsnNode last = null;
+			while(it.hasNext())
+			{
+				AbstractInsnNode curr = it.next();
+				if(curr.getOpcode()==Opcodes.POP)
+				{
+					if(last instanceof MethodInsnNode)
+					{
+						MethodInsnNode lastMeth = (MethodInsnNode)last;
+						if("java/util/TreeSet".equals(lastMeth.owner)||"java/util/Set".equals(lastMeth.owner))
+						{
+							if("remove".equals(lastMeth.name)||"add".equals(lastMeth.name))
+							{
+								it.remove();
+								InsnList insert = new InsnList();
+								insert.add(new InsnNode(Opcodes.DUP_X1));
+								m.instructions.insertBefore(last, insert);
+								insert.clear();
+								insert.add(new VarInsnNode(Opcodes.ALOAD, 0));
+								insert.add(new LdcInsnNode(lastMeth.owner+"::"+lastMeth.name));
+								insert.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "blusunrize/immersiveengineering/common/asm/ThreadLogger", "logIfFalse",
+										"(Lnet/minecraft/world/NextTickListEntry;ZLnet/minecraft/world/WorldServer;Ljava/lang/String;)V", false));
+								m.instructions.insert(last, insert);
+							}
+						}
+					}
+				}
+				last = curr;
+			}
 		};
 		transformerMap.put("net.minecraft.world.WorldServer", new MethodTransformer[]{
 			new MethodTransformer("updateBlockTick", "func_175654_a",
@@ -42,27 +74,71 @@ public class IEClassTransformer implements IClassTransformer
 					"(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/Block;II)V", addThreadingCall),
 			new MethodTransformer("getPendingBlockUpdates", "func_175712_a",
 					"(Lnet/minecraft/world/gen/structure/StructureBoundingBox;Z)Ljava/util/List;", addThreadingCall),
+				new MethodTransformer("tickUpdates", "func_72955_a", "(Z)Z", addThreadingCall),
+		});
+		Consumer<MethodNode> addChangeCall = (m) -> {
+			InsnList insert = new InsnList();
+			insert.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "blusunrize/immersiveengineering/common/asm/ThreadLogger", "verifyOrderChangeCall",
+					"()V", false));
+			m.instructions.insertBefore(m.instructions.get(0), insert);
+		};
+		transformerMap.put("net.minecraft.world.NextTickListEntry", new MethodTransformer[]{
+				new MethodTransformer("setScheduledTime", "func_77176_a",
+						"(J)Lnet/minecraft/world/NextTickListEntry;", addChangeCall),
+				new MethodTransformer("setPriority", "func_82753_a",
+						"(I)V", addChangeCall),
 		});
 	}
 
 	@Override
 	public byte[] transform(String name, String transformedName, byte[] basicClass)
 	{
-		if(basicClass!=null&&transformerMap.containsKey(transformedName))
+		if(basicClass!=null)//&&transformerMap.containsKey(transformedName))
 		{
-			MethodTransformer[] transformers = transformerMap.get(transformedName);
+			MethodTransformer[] transformers = transformerMap.getOrDefault(transformedName, new MethodTransformer[0]);
 			ClassReader reader = new ClassReader(basicClass);
 			ClassNode node = new ClassNode();
 			reader.accept(node, 0);
 
 			for(MethodNode method : node.methods)
+			{
 				for(MethodTransformer methodTransformer : transformers)
 					if((methodTransformer.functionName.equals(method.name)||methodTransformer.srgName.equals(method.name))&&methodTransformer.functionDesc.equals(method.desc))
 						methodTransformer.function.accept(method);
-
-			ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS|ClassWriter.COMPUTE_FRAMES);
-			node.accept(writer);
-			return writer.toByteArray();
+				for(AbstractInsnNode insn : method.instructions.toArray())
+				{
+					if(insn instanceof FieldInsnNode)
+					{
+						FieldInsnNode fInsn = (FieldInsnNode)insn;
+						if("field_77180_e".equals(fInsn.name))
+							System.out.println("Access to scheduledTime found: "+name+"::"+method.name+"|"+method.desc+
+									", opcode is "+fInsn.getType());
+						else if("field_82754_f".equals(fInsn.name))
+							System.out.println("Access to priority found: "+name+"::"+method.name+"|"+method.desc+
+									", opcode is "+fInsn.getType());
+						else if("field_73064_N".equals(fInsn.name))
+							System.out.println("Access to hash found: "+name+"::"+method.name+"|"+method.desc+
+									", opcode is "+fInsn.getType());
+						else if("field_73065_O".equals(fInsn.name))
+							System.out.println("Access to tree found: "+name+"::"+method.name+"|"+method.desc+
+									", opcode is "+fInsn.getType());
+					}
+					else if(insn instanceof LdcInsnNode)
+					{
+						LdcInsnNode ldc = (LdcInsnNode)insn;
+						if("field_73064_N".equals(ldc.cst))
+							System.out.println("String ref to hash found: "+name+"::"+method.name+"|"+method.desc);
+						else if("field_73065_O".equals(ldc.cst))
+							System.out.println("String ref to tree found: "+name+"::"+method.name+"|"+method.desc);
+					}
+				}
+			}
+			if(transformers.length > 0)
+			{
+				ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+				node.accept(writer);
+				return writer.toByteArray();
+			}
 		}
 		return basicClass;
 	}
--- src/main/java/blusunrize/immersiveengineering/common/asm/ThreadLogger.java
+++ src/main/java/blusunrize/immersiveengineering/common/asm/ThreadLogger.java
@@ -8,17 +8,49 @@
 
 package blusunrize.immersiveengineering.common.asm;
 
+import net.minecraft.world.NextTickListEntry;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+
 public class ThreadLogger
 {
-	private static boolean firstCall = true;
+	private static boolean firstThreadCall = true, firstChangeCall = true;
 	public static void checkForThread(String expected)
 	{
-		if (firstCall)
+		if(firstThreadCall)
 		{
 			System.out.println("Threading checks are active!");
-			firstCall = false;
+			firstThreadCall = false;
 		}
 		if(!expected.equals(Thread.currentThread().getName()))
 			Thread.dumpStack();
+	}
+
+	public static void verifyOrderChangeCall()
+	{
+		if(firstChangeCall)
+		{
+			System.out.println("Change caller checks are active!");
+			firstChangeCall = false;
+		}
+		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+		//0: getStackTrace, 1: verifyOrder, 2: set..., 3: caller
+		StackTraceElement caller = stack[3];
+		if(!"net.minecraft.world.WorldServer".equals(caller.getClassName()))
+		{
+			Thread.dumpStack();
+		}
+	}
+
+	public static void logIfFalse(NextTickListEntry e, boolean success, WorldServer world, String info)
+	{
+		if(!success)
+		{
+			Object hash = ObfuscationReflectionHelper.getPrivateValue(WorldServer.class, world,
+					"pendingTickListEntriesHashSet", "field_73064_N");
+			Object tree = ObfuscationReflectionHelper.getPrivateValue(WorldServer.class, world,
+					"pendingTickListEntriesTreeSet", "field_73065_O");
+			new RuntimeException("Failure related to "+e+" at "+info+". Hash: "+hash+", tree: "+tree).printStackTrace();
+		}
 	}
 }
